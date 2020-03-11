124d04b44e589cce22be4b4963862e5579d79c47
LowDeep <bouchikhi.taha@gmail.com>
2020-03-11 22:08:03 +0100

    Merge branch 'connection-inscription' into dev
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/progmobileproject/ConnectionActivity.java
+++ app/src/main/java/com/example/progmobileproject/ConnectionActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class ConnectionActivity extends AppCompatActivity {
 
     Button btn_inscription;
+    Button btn_connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +23,20 @@ public class ConnectionActivity extends AppCompatActivity {
         //definition bouton inscription pour rediriger vers la page d'inscription
         btn_inscription.setOnClickListener(v->{
             Intent it = new Intent(this , InscriptionActivity.class);
+            //LES METTRE SUR UN COMPTE ET AJOUTER LE COMPTE DANS LA BDD
             startActivity(it);
         });
+
+        btn_connection = (Button) findViewById(R.id.Connexion_connection);
+        btn_connection.setOnClickListener( v->{
+            Intent it = new Intent(this, PageAcceuilModeConnecte.class);
+            //RECUPERER TOUTES LES INFORMATIONS SUR LES EDIT TEXT
+            //VERIFIER QUE LE COMPTE EXISTE DEJA
+           
+
+            startActivity(it);
+        });
+
 
     }
 }
