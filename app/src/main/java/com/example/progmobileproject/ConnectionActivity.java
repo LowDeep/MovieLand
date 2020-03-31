aba3e97de1f927fd8502d8a7599bb0c6d083f36d
Ablak Hatice <ablak.hatice41@gmail.com>
2020-03-31 19:11:29 +0200

    Ajout redirection vers Youtube

--- app/src/main/java/com/example/progmobileproject/ConnectionActivity.java
+++ app/src/main/java/com/example/progmobileproject/ConnectionActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -59,6 +60,7 @@ public class ConnectionActivity extends AppCompatActivity {
             }
 
         });
+
 
 
 
